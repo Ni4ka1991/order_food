eb4c7821a04d453de634db31845ac294cdd1fa36
Ni4ka <astahova_nica@mail.ru>
2021-03-12 11:17:33 +0200

    Add file order_food.py

---
+++ order_food.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env Python3
+
+#data
+
+food_1_name     = "Pizza"
+food_1_price    = 45          #MDL
+food_1_aviable  = 5           #piece
+
+food_2_name     = "Bifsteak"
+food_2_price    = 122         #MDL
+food_2_aviable  = 2           #piece
+
+drink_1_name    = "Orange juice fresh"
+drink_1_price   = 25          #MDL
+drink_1_aviable = 1
+
+
+
+
+#logic
+
+
+
+
+
+#view
