4415348c8554d4c6180ee9132aaf1ebe5465f532
Ni4ka <astahova_nica@mail.ru>
2021-03-12 12:41:25 +0200

    Add file order_food.py

--- order_food.py
+++ order_food.py
@@ -25,10 +25,18 @@ food_1_quantity = int(input("How many " + food_1_name + " do you want?"))
 if (food_1_quantity <= food_1_aviable):
   food_1_cost = food_1_quantity * food_1_price
   print("We have enough " + food_1_name + ".")
-  print("The price of " + food_1_quantity + " will be " + food_1_cost + " MDL")
+  print("The price of " + str(food_1_quantity) + " pieces  will be " + str(food_1_cost) + " MDL")
 else:
   print("Sorry. We haven't enougt piece of " + food_1_name + ".")
-  print("The maximal quantity of " + food_1_name + " you can order is " + food_1_aviable + ".")
+  print("The maximal quantity of " + food_1_name + " you can order is " + str(food_1_aviable) + ".")
+  print("Do you whant to change your order$.")
+  
+  response = input("Type Y or N")
+  if (response == "Y"):
+    food_1_quantity = int(input("Change your order. Put here maximal quantity of " + food_1_name)
+  else:
+   print("You can order another dish. We have a lot of different dishes.")
+	
 
 
 #view
